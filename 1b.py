8f6cf55c6db323266f8ef13cd4e657932a0f15a2
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2023-12-02 13:39:33 +0000

    Update 1b.py

--- 1b.py
+++ 1b.py
@@ -2,31 +2,86 @@ import re
 import sys
 
 def firstAndLast(text):
-    first_digit = None
-    last_digit = None
+    first_digit_spelt = None
+    last_digit_spelt = None
+    first_digit_num = None
+    last_digit_num = None
+    first_digit = -1
+    last_digit = -1
     spelled_out_numbers = {
-        'one': '1', 'two': '2', 'three': '3', 'four': '4',
-        'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
-    }
+            'one': '1', 'two': '2', 'three': '3', 'four': '4',
+            'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+            }
+    numbers = {'1': 1, '2': 2, '3': 3, '4': 4,
+            '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
+            }
 
-    # Extract sequences of characters using regex
-    sequences = re.findall(r'\d+|[a-zA-Z]+', text)
-    for sequence in sequences:
-        print(sequence)
-        if sequence.isdigit():
-            rightmost_digit = int(str(sequence[-1]))  # Get the rightmost digit            
-            print(rightmost_digit)
-            if first_digit is None:
-                first_digit = int(sequence)                
-            #last_digit = int(sequence)
-            last_digit = rightmost_digit
-            
-        elif sequence.lower() in spelled_out_numbers:
-            digit = int(spelled_out_numbers[sequence.lower()])
-            if first_digit is None:
-                first_digit = digit
-            last_digit = digit
 
+    s = text
+    lastResultPos = -1
+    speltResultPos = 100000
+    digitResultPos = 100000
+## look from start of line for first number
+    #print("finding spelt nums from start")
+    for numstr in spelled_out_numbers:
+        resultPos = s.find(numstr)
+        #print(resultPos," ",  lastResultPos, " ", numstr) 
+        if resultPos >=0 and resultPos < speltResultPos:
+           first_digit_spelt=int(spelled_out_numbers[numstr])
+           #print("found number as word=", resultPos, " ", numstr)
+           lastResultPos = resultPos
+           speltResultPos = resultPos
+    #print("found first digit as word=" ,first_digit_spelt, " pos=",speltResultPos) 
+    #print("finding digit nums from start")
+    lastResultPos = -1
+    for numstr in numbers:
+        resultPos = s.find(numstr)
+        #print(resultPos, " ", numstr)
+        if resultPos >=0 and resultPos < digitResultPos:
+           first_digit_num=int(numbers[numstr])
+           #print("found number as digit=", numstr," ", resultPos)
+           lastResultPos = resultPos
+           digitResultPos = resultPos
+    #print("found first digit=" ,first_digit_num, " pos=",digitResultPos) 
+
+    if speltResultPos < digitResultPos:
+        first_digit=first_digit_spelt
+    else:
+        first_digit=first_digit_num
+
+    print("FIRST_DIGIT=",first_digit)
+## now look from end of line using find
+    #print("finding spelt from end back")
+    lastResultPos = -1
+    speltResultPos = -1
+    digitResultPos = -1
+    for numstr in spelled_out_numbers:
+        resultPos = s.find(numstr) 
+        #print(resultPos," ",  lastResultPos, " ", numstr) 
+        if resultPos >=0 and resultPos > speltResultPos:
+           last_digit_spelt=int(spelled_out_numbers[numstr])
+           #print("found number as word pos=", resultPos, " num=", numstr)
+           lastResultPos = resultPos
+           speltResultPos = resultPos
+    #print("found last digit as word=" ,last_digit_spelt) 
+    #print("finding digit nums from end")
+    lastResultPos = -1
+    for numstr in numbers:
+        resultPos = s.find(numstr)
+        #print(resultPos," ",  lastResultPos, " ", numstr) 
+        if resultPos >=0 and resultPos > digitResultPos:
+           last_digit_num=int(numbers[numstr])
+           #print("found number as digit, pos=", resultPos, " num=", numstr)
+           lastResultPos = resultPos
+           digitResultPos = resultPos
+
+    #print("found last digit=" ,last_digit_num) 
+    if speltResultPos > digitResultPos:
+        last_digit=last_digit_spelt
+    else:
+        last_digit=last_digit_num
+
+    print("LAST_DIGIT=",last_digit)
     return first_digit, last_digit
 
 
@@ -40,21 +95,17 @@ if len(sys.argv)==2:
     filename = sys.argv[1]
     inputFile = open(filename, "r")
 else:
-   print ("usage: " + sys.argv[0] + " <input filename>")
-   sys.exit(1)
-   
+    print ("usage: " + sys.argv[0] + " <input filename>")
+    sys.exit(1)
+
 lines = inputFile.readlines()
 total = 0
 for line in lines:
     print(line)
-    print(firstAndLast(line))
     first_digit, last_digit = firstAndLast(line)
-    if (first_digit!=None):
-        print ("first number = ", first_digit);
-        total += first_digit
-    if (last_digit!=None):    
-        print ("last number = ", last_digit);
-        total += last_digit
-   
+    if ((first_digit!=None) and (last_digit!=None)):    
+        total += (first_digit*10) + last_digit
+        print ("added ", (first_digit*10) + last_digit)
+
 print ("total=", total)
-    
+
