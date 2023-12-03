7178ad0181f225bba62a7f67dc837e234b370d06
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2023-12-03 05:31:12 +0000

    Update 3.py

--- 3.py
+++ 3.py
@@ -3,21 +3,43 @@ import sys    # system package
 import math   
 
 # advent of code 2023 day 3
-
+runOnPc  = False
+lines = " "
 # Read command line args
-if len(sys.argv)==2:
-    filename = sys.argv[1]
-    inputFile = open(filename, "r")
+if (runOnPc == True):
+    if len(sys.argv)==2:
+        filename = sys.argv[1]
+        inputFile = open(filename, "r")
+        lines = inputFile.readlines()
+    else:
+        print ("usage: " + sys.argv[0] + " <input filename>")
+        sys.exit(1)
 else:
-    print ("usage: " + sys.argv[0] + " <input filename>")
-    sys.exit(1)
-
-lines = inputFile.readlines()
+    lines=("467..114..",
+           "...*......",
+           "..35..633.",
+           "......#...",
+           "617*......",
+           ".....+.58.",
+           "..592.....",
+           "......755.",
+           "...$.*....",
+           ".664.598..")
 result = 0
 
 #process the lines
-for line in lines:
-                        
+lnc=0   #current line
+lnn=1   #next
+maxl=lines.len()
+
+while lnn<maxl:
+   line1=lines[lnc]
+   nums= [int(s) for s in line1.split() if s.isdigit()]
+ 
+   print(line1)
+   priny(nums)     
+   lnn+=1   
+              
 print (result)
     
     
