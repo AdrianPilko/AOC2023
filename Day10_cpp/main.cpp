9a776694e87f04ec4b448ae2cd851f70d8c07c63
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-12-10 10:37:12 +0000

    Update main.cpp

--- Day10_cpp/main.cpp
+++ Day10_cpp/main.cpp
@@ -5,6 +5,8 @@
 #include <map>
 
 
+std::string filenameStr = "8test.txt";
+//std::string filenameStr = "8.txt";
 
 /* 
 .....
@@ -14,7 +16,7 @@
 ..... 
 */
 
-std::string filenameStr = "8.txt";
+typedef enum e_directnV {eup = 0,edown, eleft, eright} t_directn; 
 
 bool getLineFromFile(std::ifstream & fileStream, std::string &retLine ) 
 { 
@@ -51,11 +53,35 @@ void getLongestLine(int & longest, int & numLines)
    inputFile.close();
 }
 
+bool isValidDirection(t_directn di, char theNextSymbol)
+{
+  bool rv = false;
+  switch (theNextSymbol)
+  {
+    case '|' : if ((di == eup) || (di == edown)) rv = true;     break;
+    case '-' : if ((di == eleft) || (di == eright)) rv = true; break;
+    case '7' : if ((di == eleft) || (di == edown)) rv = true; break;
+    case 'J' : if ((di == eleft) || (di == eup)) rv = true; break;
+    case 'F' : if ((di == eright) || (di == edown)) rv = true; break;
+    case 'L' : if ((di == eright) || (di == edown)) rv = true; break;
+    case 'S' : // should never happen but catch just in case
+              std::cout <<"got back to starting position!" << std::endl; rv = false; break;
+    default: rv = false;  break;
+    break;
+  };    
+  return rv;
+}
+
 main()
 {
+  std::cout << " entering main " << std::endl;
+  
   int longestLine;
   int numLines;
+  
   getLongestLine(longestLine, numLines);
+
+  std::cout << " got dimensions rows=" << numLines << " cols=" << longestLine << std::endl;
 
   std::vector<std::vector<char>> data(numLines, std::vector<char>(longestLine));
 
@@ -66,6 +92,8 @@ main()
       data[l][c] = '.';  // prepoulate all with '.', the real data has all different line lengths
     }
   }
+  
+  std::cout << " populated vector " << std::endl;
   
   std::ifstream inputFile(filenameStr.c_str()); // Replace "filename.txt" with your file's name
   std::string line;
@@ -91,6 +119,83 @@ main()
 
   inputFile.close(); // Close the file when done reading
   
+  // starting at rowS, ColS navigate the vector until can't go any further
+  
+  // choose a starting direction
+  t_directn currentDir;
+  t_directn lastDir;
+  char nextChar;
+  int row = rowS;
+  int col = colS;
+  
+  int nextRow = 0;
+  int nextCol = 0;
+  
+  int triedEveryDirection = 0;
+  currentDir = eup;
+  lastDir = eleft;
+  
+  std::cout << " before while loop , currentDir=" << currentDir << ", last=" << lastDir << std::endl;
+  
+  while (lastDir != currentDir)
+  {
+    if ((currentDir == eleft) && (col > 0)) 
+    { 
+      nextChar = data[row][col-1]; 
+      nextRow = row; 
+      nextCol = col-1; 
+      std::cout << " trying left,";
+    }  
+    if ((currentDir == eright) && (col < longestLine-1))
+    { 
+      nextChar = data[row][col+1];
+      nextRow = row; 
+      nextCol = col+1;
+      std::cout << " trying right,";
+    } 
+    if ((currentDir == eup) && (row > 0)) 
+    { 
+      nextChar = data[row-1][col];
+      nextRow = row-1; 
+      nextCol = col;
+      std::cout << " trying up,";
+    }  
+    if ((currentDir == edown) && (row < numLines-1)) {
+      nextChar = data[row+1][col];
+      nextRow = row+1; 
+      nextCol = col;    
+      std::cout << " trying down,";
+    }  
+    
+    if (isValidDirection(currentDir, nextChar))
+    {
+      std::cout << "valid from " << row << "," << col << " " ;
+      row = nextRow;
+      col = nextCol;
+      std::cout << "to " << row << "," << col << std::endl;
+      triedEveryDirection = 0;
+      currentDir = (t_directn)triedEveryDirection;
+    }
+    else
+    {
+      std::cout << " not valid from " << row << "," << col << " to " << nextRow << "," << nextCol << std::endl;      
+      triedEveryDirection++;
+      
+      if (triedEveryDirection <= 3)
+      {
+          currentDir = (t_directn)triedEveryDirection;        
+      }
+      else
+      {
+        triedEveryDirection = 0;
+        lastDir = currentDir;
+        currentDir = (t_directn)triedEveryDirection;
+      }
+    }
+    std::cout << " pause, press return" << std::endl;
+    std::system("pause");
+  }
+  std::cout << " pause, press return" << std::endl;
   std::system("pause");
   return 0;
 }
